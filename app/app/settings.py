91fe6bf057a5bb07716a7a0aa3f69926fc04c126
hai huynh <hai.huynhngoc@paradox.ai>
2022-12-11 17:19:25 +0700

    admin

--- app/app/settings.py
+++ app/app/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core', 
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +120,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
